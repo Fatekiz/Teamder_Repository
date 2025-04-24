7a76e8232dce069bb412c41966de4a13dcb54ced
Claudio Matias Uribe Mansilla <67400635+PeepoClown1412@users.noreply.github.com>
2025-04-24 18:36:39 -0400

    Boton Foro

--- main_window.py
+++ main_window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from profile_window import ProfileWindow
+from foro_window import ForoWindow
 
 class MainWindow:
     def __init__(self, master, usuario):
@@ -14,10 +15,13 @@ class MainWindow:
         label_bienvenida.pack(pady=20)
 
         tk.Button(master, text="Ajustes de Perfil", command=self.abrir_perfil).pack(pady=10)
-
+        tk.Button(master, text="Acceder al Foro", command=self.abrir_foro).pack(pady=10)
+        
         # Aquí irán más elementos para buscar equipos o juegos
         label_info = tk.Label(master, text="(Aquí puedes buscar jugadores, crear grupo, etc...)")
         label_info.pack()
 
     def abrir_perfil(self):
         ProfileWindow(tk.Toplevel(self.master), self.usuario)
+    def abrir_foro(self):
+        ForoWindow(tk.Toplevel(self.master), self.usuario)
